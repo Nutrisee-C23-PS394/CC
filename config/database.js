f5c2b374499e32391e4eb5bc9b012abfa30d81f7
M. Jalal Harits <jalal.harits@gmail.com>
2023-06-12 18:43:47 +0800

    i don't know why this happen

--- config/database.js
+++ config/database.js
@@ -2,13 +2,16 @@ const mysql = require('mysql');
 const util = require('util');
 
 const db = mysql.createConnection({
-  host: 'url or ip host',
-  user: 'user DB',
-  password: 'insert Password',
-  database: 'which database',
+  host: 'localhost',
+  user: 'root',
+  password: 'Ajak1@2134',
+  database: 'nutrisee',
 });
 
-// db.query = util.promisify(db.query).bind(db);
+db.beginTransaction = util.promisify(db.beginTransaction).bind(db);
+db.query = util.promisify(db.query).bind(db);
+db.commit = util.promisify(db.commit).bind(db);
+db.rollback = util.promisify(db.rollback).bind(db);
 
 db.connect((err) => {
   if (err) {
